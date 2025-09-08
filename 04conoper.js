fa0c31070e276bd3e2da9b6b84c6a0951f34aa9b
@laxman16 <laxman@valueinnovationlabs.com>
2025-09-09 00:22:19 +0530

    text

---
+++ 04conoper.js
@@ -0,0 +1,63 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log( typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1, false => 0
+
+let isLoggedIN = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIN);
+
+console.log(booleanIsLoggedIn);
+
+// true => 1 ; false => 0
+// "" => false;
+// "laxman" => true;
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// ****************************** Operations ********************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// let str = "Hello";
+// let str1 = " India";
+// let str2 = str + str1;
+// console.log(str2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+ "");
+let num1, num2, num3
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter ++;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
---
+++ datatypesum.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types: String, Number, Boolear, null, undifined, Symbol, BigInt;
+
+const scope = 100;
+const valueScope = 100.3;
+const isLoggedIN = false;
+const outsiteTemp = null;
+let useEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId);
+
+const bigNumber = 555555555555587455n;
+
+const heros = ["Ram", "Mukesh", "Rohit", "Monika"];
+let MyObj = {
+    name: "Arun",
+    age: 33,
+    className : "MCA"
+}
+
+const myFunction = function(){
+    console.log("hello World");
+}
+
+// Reference (Non -Primitive)
+
+// Array, Objects , Functions
+
